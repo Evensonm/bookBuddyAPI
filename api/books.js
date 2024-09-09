1e322f1384e173fc733115dbdadfb70138640bfb
evensonm <evensonm1@gmail.com>
2024-09-09 11:58:17 -0400

    add Three

--- api/books.js
+++ api/books.js
@@ -1,9 +1,103 @@
 const express = require("express");
+const {
+  getBooks,
+  getBook,
+  createBook,
+  deleteBook,
+  updateBook,
+} = require("../db/books");
 
 const bookRouter = express.Router();
-// {basURL}/users/me
-bookRouter.get("/", (req,res)=>{
-    res.send("Here are the books");
+
+const {requireUser} = require("./utils");
+// {basURL}/api/books
+
+bookRouter.get("/", async (req, res) => {
+  try {
+    const results = await getBooks();
+    res.send(results);
+  } catch (err) {
+    next(err);
+  }
 });
 
-module.exports = bookRouter
+//{baseURL}/api/books/id
+bookRouter.get("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    console.log(id);
+    if (isNaN(id) || req.params.id === " ") {
+      next({
+        name: "invalid ID Format",
+        message: " The Provided Request parameter is not a valid id",
+      });
+      return;
+    }
+    const result = await getBook(id);
+    if (!result) {
+      next({ name: "not found", message: "No matching book found" });
+      return;
+    }
+    res.send(result);
+  } catch (err) {
+    next(err);
+}});
+//{baseUrl}/api/books POST function
+bookRouter.post("/", requireUser, async (req, res) => {
+  try {
+    const result = await createBook(req.body);
+    console.log(result);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+bookRouter.delete("/:id", requireUser, async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const result = await deleteBook(id);
+    console.log(result);
+    res.send({ message: "Book Deleted", id: result });
+  } catch (err) {
+    res.send({ err, message: "something went wrong" });
+  }
+});
+
+bookRouter.patch("/:id", requireUser, async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    console.log(id);
+    if (isNaN(id) || req.params.id === " ") {
+      next({
+        name: "invalid ID Format",
+        message: " The Provided Request parameter is not a valid id",
+      });
+      return;
+    }
+    const result = await getBook(id);
+    if (!result) {
+      next({ name: "not found", message: " no matching book found" });
+      return;
+    } else {
+      const updated = await updateBook(req.params.id, req.body.available);
+      if (updated) {
+        res.send({
+          message: " updated successfully",
+          updated,
+        });
+      }else{
+        next({
+          name:"UpdateError",
+          message:"There was an error updating this book.",
+        });
+        return;
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = bookRouter;
--- index.js
+++ index.js
@@ -9,15 +9,31 @@ const client = require("./db/client");
 
 client.connect();
 
+// console.log(process.env.JWT_SECRET);
+
 app.use(express.json());
 // /api -> send to /api/index.js
 // express will look for index.js folder.
 // that is why duplicate name under api folder
 app.use("/api", require("./api"));
+// app.use("/db", require("./db"));
 
 //SENDING MESSAGE TO BROWSER INDICATING PAGE IS LIVE AND FROM WHERE
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+app.get("*", (req,res)=>{
+  res.status(404).send({error:"404 - Not Found", 
+    message:"No route found for requested URL"})
+});
+
+app.use((err, req, res, next) => {
+  console.log("error", error);
+  if(res.statusCode < 400) res.status(500);
+  res.send({message : error.message,
+    name: error.name,
+  });
 });
 
 //LOG THAT PORT IS ACTIVE
